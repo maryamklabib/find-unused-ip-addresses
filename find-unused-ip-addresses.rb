f1b389a4d606a7a3a607e1f2ff5f4aff5687039a
Maryam Labib <maryamlabib@berkeley.edu>
2016-07-26 22:05:04 -0700

    this is the rough idea

--- find-unused-ip-addresses.rb
+++ find-unused-ip-addresses.rb
@@ -1,1 +1,19 @@
 #!/usr/bin/env ruby
+
+#loop through each ip we are passeputs "I was passed: "
+ARGV.each do |value|
+  puts value
+end
+
+
+def list_of_ips_in_range(starting_ip, ending_ip)
+	base_ip = start.ip.split('.')[:3]
+	start = start.split('.')[3]
+	end = ending_ip.split('.')[3]
+	if start < end
+		(start..end).step(1) do |n|
+   		 # ping "#{base_ip}.#{n}"
+		end
+	end
+end
+
